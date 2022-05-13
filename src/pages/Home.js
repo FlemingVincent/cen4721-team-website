785681dc4304eb9064a097320ebd9417211a77c9
ZanderBournand <z.bournand@gmail.com>
2022-05-13 14:40:58 -0400

    Added My (Zander's) Bio Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,13 @@ function Home() {
           interests="UI design, frontend development, and mobile"
           bio="After graduating, Vincent plans on becoming a frontend software engineer. He spends most of his time designing and developing projects to better his abilities using programs such as Figma, and frameworks such as React and React Native. Vincent is currently a mobile development intern at FLYJETS, helping develop and launch their app this summer."
         />
+        <BioCard
+          firstName="zander"
+          lastName="bournand"
+          major="computer science"
+          interests="Full Stack Mobile and Web Development"
+          bio="Zander is currently a Junior at The University of Florida, studying Computer Science. Most of his schedule consists of school work and personal development of full stack projects to improve his skills as a software engineer in general. Following graduation, Zander plans on working as a Software Engineer within a tech company to start off his proffesional life. "
+        />
       </div>
     </div>
   );
