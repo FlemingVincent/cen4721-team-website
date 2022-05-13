62da128bce9e290ffabaa2a0136fbd3011235d13
Vincent <flemingvincent@ufl.edu>
2022-05-13 00:19:35 -0400

    added bio card component and updated readme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,22 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="px-4 py-8 shadow-md rounded-b-lg flex flex-row justify-between text-lg font-medium">
+    <div className="px-4 py-8 shadow-lg flex flex-row justify-between text-lg font-medium">
       <div>
-        <Link to="/">Home</Link>
+        <Link to="/" className="hover:text-[#0021A5]">
+          Home
+        </Link>
       </div>
       <div className="flex flex-row gap-8">
-        <Link to="ProjectOne">Project One</Link>
-        <Link to="ProjectTwo">Project Two</Link>
-        <Link to="ProjectThree">Project Three</Link>
+        <Link to="ProjectOne" className="hover:text-[#0021A5]">
+          Project One
+        </Link>
+        <Link to="ProjectTwo" className="hover:text-[#0021A5]">
+          Project Two
+        </Link>
+        <Link to="ProjectThree" className="hover:text-[#0021A5]">
+          Project Three
+        </Link>
       </div>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 
+import BioCard from "../components/BioCard";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 py-8">
+      <h1 className="text-4xl my-8 flex justify-center">Meet the Team</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <BioCard
+          firstName="vincent"
+          lastName="fleming"
+          major="computer science"
+          interests="UI design, frontend development, and mobile"
+          bio="After graduating, Vincent plans on becoming a frontend software engineer. He spends most of his time designing and developing projects to better his abilities using programs such as Figma, and frameworks such as React and React Native. Vincent is currently a mobile development intern at FLYJETS, helping develop and launch their app this summer."
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
