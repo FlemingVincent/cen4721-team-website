97c5ee7ae23ffae99b12970c0545c6f9efd75207
ZanderBournand <z.bournand@gmail.com>
2022-05-28 18:08:01 -0400

    added to UCD

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,25 +1,31 @@
+import "./ucd.css"
 import needs from "../images/needs.png"
 import needsG from "../images/needs_grouped.png"
 import solution from "../images/solutions.png"
 import {useState, useEffect} from "react"
 
 
-const images = [needsG, solution];
+export default function Images(props) {
 
-export default function Images() {
+    const images = props.images
     const [currentImage, setCurrentImage] = useState(null);
 
     useEffect(() => {
+
+        let i = 0;
+
         const intervalId = setInterval(() => {
-            setCurrentImage(images[Math.floor(Math.random() * images.length)]);
+            setCurrentImage(images[i]);
+            i = (i == images.length - 1) ? 0 : i+1;
+            console.log(currentImage.offsetHeight)
         }, 5000)
 
         return () => clearInterval(intervalId);
     }, [])
 
     return (
-        <div>
-            <img src={currentImage} className="h-60 w-65 float-left"/>
+        <div className="bg-gray">
+            <img src={currentImage} className="object-center"/>
         </div>
     )
 }
